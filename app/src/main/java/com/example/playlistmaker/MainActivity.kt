4630641ed5a568adec38e4e3790012526f13bc32
Nikolai Matveenkov <matdrik@icloud.com>
2025-12-02 21:49:35 +0200

    feat: практика 6

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        //  Нажатие на картинку вторым способом
+        val image = findViewById<ImageView>(R.id.poster)
+        image.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на картинку", Toast.LENGTH_SHORT).show()
+            image.setBackgroundColor(getColor(R.color.black))
+            image.scaleType = ImageView.ScaleType.CENTER_CROP
+            image.setImageResource(R.drawable.poster)
         }
+
+        //  Нажатие на кнопку вторым способом
+        val buttonNo = findViewById<Button>(R.id.button_no)
+        buttonNo.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на НЕТ", Toast.LENGTH_SHORT).show()
+            image.setBackgroundColor(getColor(R.color.black))
+            image.scaleType = ImageView.ScaleType.CENTER_CROP
+            image.setImageResource(R.drawable.poster)
+        }
+
+        //  Нажатие на кнопку первым способом
+        val buttonYes = findViewById<Button>(R.id.button_yes)
+        val buttonYesClickListener: View.OnClickListener = object : View.OnClickListener { override fun onClick(v: View?) {
+            Toast.makeText(this@MainActivity, "Нажали на ДА", Toast.LENGTH_SHORT).show()
+        } }
+        buttonYes.setOnClickListener(buttonYesClickListener)
     }
 }
